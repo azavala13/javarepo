62519d4f03547067cf568e7d7856ffd0f72d1e27
azavala13 <147428725+azavala13@users.noreply.github.com>
2023-10-13 15:48:18 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://javabucketcodebuild/java-build-project/LoginWebApp-1.war
-sudo mv /java-build-project/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+aws s3 cp s3://javabucketcodebuild/My-Java-Pipeline/LoginWebApp-1.war
+sudo mv /javabucketcodebuild/My-Java-Pipeline/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
