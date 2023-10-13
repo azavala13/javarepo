e32d2deb8aece585be9d6817022d6cf90b243318
Zavala <adzavala@deloitte.com>
2023-10-12 23:37:12 -0700

    new commit

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-aws s3 cp s3://java-artifacts-devops4solutions/target/LoginWebApp-1.war/sample-java-projects/LoginWebApp-1.war /tmp
-sudo mv /tmp/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
+aws s3 cp s3://javabucketcodebuild/java-build-project/LoginWebApp-1.war
+sudo mv /java-build-project/LoginWebApp-1.war /usr/share/tomcat/webapps/LoginWebApp-1.war
 sudo service tomcat restart
